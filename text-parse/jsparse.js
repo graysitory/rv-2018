cc2d32609e791a4a992b81c6874345470e60b46a
graysitory <gbarber@pnca.edu>
2018-10-06 14:18:22 -0700

    Recalculates stage height and width on window resize.

--- text-parse/jsparse.js
+++ text-parse/jsparse.js
@@ -7,33 +7,34 @@ var walk = require('walk');
 //https://stackoverflow.com/questions/2727167/how-do-you-get-a-list-of-the-names-of-all-files-present-in-a-directory-in-node-j
 
 
-var inputFiles = [];
-var inputFilesPath = [];
 
 var getFilesToParse = walk.walk('./input', {followLinks: false});
 
 getFilesToParse.on('file', function(root, stat, next) {
   inputFiles.push(stat.name); //just return the filename
   // var fileToParse = root + '/' + stat.name;
-  // makeProjectButton(fileToParse)
+
+  var fileToParse = "/Users/Gray/Documents/Development/railvolution/codepen/text-parse/input/" + stat.name;
+
+  makeProjectButton(fileToParse)
   next();
 });
 
-getFilesToParse.on('end', function() {
-
-  var fullPath = "/Users/Gray/Documents/Development/railvolution/codepen/text-parse/input/"
-
-  for (var i = 0; i <= inputFiles.length; i++) {
-    var newFile = inputFiles[i];
-    var inputPath = fullPath + newFile
-    console.log(inputPath.toString())
-
-    inputPath = inputPath.toString();
-    console.log(makeProjectButton(inputPath))
-    //console.log(makeProjectButton(inputPath))
-  }
-
-})
+// getFilesToParse.on('end', function() {
+//
+//   var fullPath = "/Users/Gray/Documents/Development/railvolution/codepen/text-parse/input/"
+//
+//   for (var i = 0; i <= inputFiles.length; i++) {
+//     var newFile = inputFiles[i];
+//     var inputPath = fullPath + newFile
+//     console.log(inputPath.toString())
+//
+//     inputPath = inputPath.toString();
+//     console.log(makeProjectButton(inputPath))
+//     //console.log(makeProjectButton(inputPath))
+//   }
+//
+// })
 
 
 // for (var e = 0; e <= inputFilesPath.length; e++) {
@@ -41,10 +42,23 @@ getFilesToParse.on('end', function() {
 // }
 
 
-function makeProjectButton(fileToParse) {
 
-var fileToParse = fileToParse;
-console.log(fileToParse)
+
+var input = process.argv[2];
+var output = input + '.w'
+
+
+fs.readFile(input, 'utf-8', function(err, text) {
+  if (err) throw err;
+
+  var buttonText = makeProjectButton(text)
+  console.log(buttonText)
+
+  // fs.writeFile(output, buttonText)
+
+})
+
+function makeProjectButton(fileToParse) {
 
 var projID;
 var projImages = [];
@@ -61,15 +75,15 @@ var projTags;
 
 var imgPath = "blah";
 
-var textToParse = fs.readFileSync(fileToParse, "utf8").split('\n');
+// var textToParse = fs.readFileSync(fileToParse, "utf8").split('\n');
 
 
 
 for (var i = 0; i <= textToParse.length; i++) {
 
-  if (textToParse[i] === "%01 PROJECT ID") {
+  if (textToParse[i] === "#01 PROJECT ID") {
     projID = textToParse[i + 1];
-  } else if (textToParse[i] === "%02 PROJECT IMAGES") {
+  } else if (textToParse[i] === "#02 PROJECT IMAGES") {
     for (var e = 1; e < textToParse.length; e++) { // now loop though indices until a "".
       if (textToParse[i + e] !== "") {
         projImages.push(textToParse[i + e]);
@@ -77,26 +91,26 @@ for (var i = 0; i <= textToParse.length; i++) {
         break;
       }
     }
-  } else if (textToParse[i] === "%03 THUMBNAIL") {
+  } else if (textToParse[i] === "#03 THUMBNAIL") {
     projThumbnail = textToParse[i + 1];
-  } else if (textToParse[i] === "%04 PROJECT LONG NAME") {
+  } else if (textToParse[i] === "#04 PROJECT LONG NAME") {
     projLongName = textToParse[i + 1];
-  } else if (textToParse[i] === "%05 PROJECT SHORT NAME") {
+  } else if (textToParse[i] === "#05 PROJECT SHORT NAME") {
     projShortName = textToParse[i + 1];
-  } else if (textToParse[i] === "%06 CLIENT LONG NAME") {
+  } else if (textToParse[i] === "#06 CLIENT LONG NAME") {
     clientLongName = textToParse[i + 1];
-  } else if (textToParse[i] === "%07 CLIENT SHORT NAME") {
+  } else if (textToParse[i] === "#07 CLIENT SHORT NAME") {
     clientShortName = textToParse[i + 1];
-  } else if (textToParse[i] === "%08 LOCATION") {
+  } else if (textToParse[i] === "#08 LOCATION") {
     projLocation = textToParse[i + 1];
-  } else if (textToParse[i] === "%09 SIZE") {
+  } else if (textToParse[i] === "#09 SIZE") {
     projSize = textToParse[i + 1];
-  } else if (textToParse[i] === "%10 ZGF ROLE") {
+  } else if (textToParse[i] === "#10 ZGF ROLE") {
     projZGFRole = textToParse[i + 1];
-  } else if (textToParse[i] === "%11 STATUS") {
+  } else if (textToParse[i] === "#11 STATUS") {
     projStatus = textToParse[i + 1];
 
-  } else if (textToParse[i] === "%12 TAGS") {
+  } else if (textToParse[i] === "#12 TAGS") {
     projTags = textToParse[i + 1];
 
   }
@@ -105,7 +119,7 @@ for (var i = 0; i <= textToParse.length; i++) {
 
 
 
- projImages = projImages.join('%')
+ projImages = projImages.join('#')
 
 
  var output = '<div class="project-button" id="' + projID + '" data-img="' + projImages + '" data-thumbnail="' + projThumbnail + '" data-namelong="' + projLongName + '" data-nameshort="' + projShortName + '" data-clientlong="' + clientLongName + '" data-clientshort="' + clientShortName + '" data-projlocation="' + projLocation + '" data-projsize="' + projSize + '" data-zgfrole="' + projZGFRole + '" data-projstatus="' + projStatus + '" data-tag="all ' + projTags + '" style="background-image: url(' + imgPath + projThumbnail + '); background-position: center; background-size: cover;"> </div>';
@@ -114,7 +128,6 @@ for (var i = 0; i <= textToParse.length; i++) {
 
   output = output.replace(/[']/g, '"')
 
-  console.log(output)
 
 
   }
